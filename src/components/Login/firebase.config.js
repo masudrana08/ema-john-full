530204ca2f0a31fcdd592321edbf258005f1371d
masudrana08 <rana028511@gmail.com>
2020-10-01 04:41:54 +0600

    ema john finally done with node mongo

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB7MCYUQuJEAkdcXIztesFK5njcs65hz6s",
+  authDomain: "ema-john-node-mongo.firebaseapp.com",
+  databaseURL: "https://ema-john-node-mongo.firebaseio.com",
+  projectId: "ema-john-node-mongo",
+  storageBucket: "ema-john-node-mongo.appspot.com",
+  messagingSenderId: "843477592360",
+  appId: "1:843477592360:web:b15b0d186673130948481d"
+};
+  export default firebaseConfig;
